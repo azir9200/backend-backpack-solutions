3789a43b889aa0388e45de8a82b90389535a1584
Azir@9200 <aziruddin83@hotmail.com>
2025-05-08 10:48:09 +0100

    created class function

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  }
+  return input.toUpperCase();
+}
+
+// Example usage:
+// console.log(formatString("Hello"));
+// console.log(formatString("Hello", true));
+// console.log(formatString("Hello", false));
+
+// //  number 2
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+// console.log(filterByRating(books));
+
+// //  number 3
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+// console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+// //  number 4
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  public getInfo(): string {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+// console.log(myCar.getInfo());
+// console.log(myCar.getModel());
+
+
+
+
+//number 5
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (typeof value === "number") {
+    return value * 2;
+  }
+ 
+  throw new Error("Invalid input type");
+}
